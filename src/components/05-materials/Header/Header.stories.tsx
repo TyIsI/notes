cf51886199d65e56c304559b061367d5c8f7240e
Ty Eggen <tycho.eggen@gmail.com>
2025-08-27 19:41:56 +0000

    feat: 🎸 new Header material

--- src/components/05-materials/Header/Header.stories.tsx
+++ src/components/05-materials/Header/Header.stories.tsx
@@ -1,19 +1,13 @@
-import type { Meta } from '@storybook/react-vite'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 
 import Header from './Header'
 
 const meta = {
-    name: '05-materials/Header',
     component: Header
-} satisfies Meta<typeof Button>
+} satisfies Meta<typeof Header>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {
-    args: {
-        primary: true,
-        label: 'Header'
-    }
-}
+export const Primary: Story = {}
