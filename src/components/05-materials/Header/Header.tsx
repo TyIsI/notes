aadd3326805dc9bc48467c2ebfc81cc38783e318
Ty Eggen <tycho.eggen@gmail.com>
2025-08-28 15:39:18 +0000

    feat: 🎸 improved light-dark handling in Header

--- src/components/05-materials/Header/Header.tsx
+++ src/components/05-materials/Header/Header.tsx
@@ -7,7 +7,7 @@ import type { HeaderProps } from './Header.types'
 import ThemeSelector from '@/components/03-particles/ThemeSelector/ThemeSelector'
 
 const Header: FC<HeaderProps> = () => (
-    <div className='shadow-nav-dark dark:shadow-nav-light fixed top-0 right-0 left-0 z-10 grid h-10 grid-cols-2 *:w-fit *:min-w-32 *:p-1.5 *:align-middle'>
+    <div className='shadow-nav-light dark:shadow-nav-dark fixed top-0 right-0 left-0 z-10 grid h-10 grid-cols-2 *:w-fit *:min-w-32 *:p-1.5 *:align-middle'>
         <div className='justify-self-start'>
             <Link to={import.meta.env.BASE_URL}>Ty&apos;s Notes</Link>
         </div>
