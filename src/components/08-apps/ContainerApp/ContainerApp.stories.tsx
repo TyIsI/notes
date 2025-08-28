bd6e63a26a180ea6bfabc7e16b538649ae25c1ba
Ty Eggen <tycho.eggen@gmail.com>
2025-08-28 15:31:03 +0000

    refactor: :art: moved global app code into separate components

---
+++ src/components/08-apps/ContainerApp/ContainerApp.stories.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import ContainerApp from './ContainerApp'
+
+export default {
+    title: 'ContainerApp'
+}
+
+export const Default = () => <ContainerApp />
+
+Default.story = {
+    name: 'default'
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,12 @@
-import '@/styles/index.css'
+import { createRootRoute } from '@tanstack/react-router'
 
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-
-import Header from '@/components/05-materials/Header/Header'
-import MainSideBarMenu from '@/components/05-materials/MainSideBarMenu/MainSideBarMenu'
+import App from '@/components/08-apps/App/App'
+import ContainerApp from '@/components/08-apps/ContainerApp/ContainerApp'
 
 export const Route = createRootRoute({
     component: () => (
-        <div className='container'>
-            <Header />
-            <div className='relative flex flex-row'>
-                <div className='dark:shadow-sidemenu-dark shadow-sidemenu-light fixed top-20 bottom-0 left-0 flex h-full min-h-dvh w-56 gap-2 p-2'>
-                    <MainSideBarMenu />
-                </div>
-                <div className='dark:shadow-main-dark shadow-main-light relative right-4 -mt-8 ml-56 min-h-1/2 w-auto min-w-7/8 border'>
-                    <Outlet />
-                </div>
-            </div>
-            {import.meta.env.DEV ? <TanStackRouterDevtools /> : null}
-        </div>
+        <ContainerApp>
+            <App />
+        </ContainerApp>
     )
 })
