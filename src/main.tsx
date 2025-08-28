6579bc50ed96b312f4a129c58ee5e3b9a4dd2112
Ty Eggen <tycho.eggen@gmail.com>
2025-08-28 15:35:34 +0000

    refactor: 💡 moved global app code into separate components #2

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 
 import { createRouter, RouterProvider } from '@tanstack/react-router'
 import ReactDOM from 'react-dom/client'
-import { SWRConfig } from 'swr'
 
 import ErrorPage from '@/components/07-pages/ErrorPage/ErrorPage'
 import LoadingPage from '@/components/07-pages/LoadingPage/LoadingPage'
-import DocsIndexProvider from '@/components/08-providers/DocsIndexProvider/DocsIndexProvider'
-import ThemeProvider from '@/components/08-providers/ThemeProvider/ThemeProvider'
 
 import { getRootElement } from './main.ui'
 import { routeTree } from './routeTree.gen'
@@ -41,25 +38,6 @@ const root = ReactDOM.createRoot(rootElement)
 
 root.render(
     <StrictMode>
-        <SWRConfig
-            value={{
-                refreshInterval: 3000,
-                fetcher: async (
-                    resource: RequestInfo | URL,
-                    init?: RequestInit
-                ) =>
-                    // eslint-disable-next-line @typescript-eslint/no-unsafe-return -- YOLO
-                    await fetch(resource, init).then(
-                        // eslint-disable-next-line @typescript-eslint/no-unsafe-return -- YOLO
-                        async (res) => await res.json()
-                    )
-            }}
-        >
-            <ThemeProvider>
-                <DocsIndexProvider>
-                    <RouterProvider router={router} />
-                </DocsIndexProvider>
-            </ThemeProvider>
-        </SWRConfig>
+        <RouterProvider router={router} />
     </StrictMode>
 )
