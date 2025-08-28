3539014170437ed5de10efd952376a9f1ee6deff
Ty Eggen <tycho.eggen@gmail.com>
2025-08-28 15:38:29 +0000

    feat: 🎸 implement loading markdown docs from KnowledgeBasePage

--- src/components/07-pages/KnowledgeBasePage/KnowledgeBasePage.tsx
+++ src/components/07-pages/KnowledgeBasePage/KnowledgeBasePage.tsx
@@ -1,9 +1,51 @@
-import type { FC } from 'react'
+import { useMemo, type FC } from 'react'
+
+import { useLocation } from '@tanstack/react-router'
+import Markdown from 'markdown-to-jsx'
+import useSWR from 'swr'
 
 import type { KnowledgeBasePageProps } from './KnowledgeBasePage.types'
 
-const KnowledgeBasePage: FC<KnowledgeBasePageProps> = () => (
-    <div>KnowledgeBasePage Component</div>
-)
+import { useDocsIndexContext } from '@/components/08-providers/DocsIndexProvider/DocsIndexProvider.context'
+
+import type { FetcherException } from '@/lib/exceptions/FetcherException'
+
+import ErrorPage from '../ErrorPage/ErrorPage'
+import LoadingPage from '../LoadingPage/LoadingPage'
+
+const KnowledgeBasePage: FC<KnowledgeBasePageProps> = () => {
+    const docsIndex = useDocsIndexContext()
+
+    const location = useLocation()
+
+    const pathParts = useMemo(
+        () =>
+            location.pathname.replace(import.meta.env.BASE_URL, '').split('/'),
+        [location]
+    )
+
+    const articleFile = useMemo(() => {
+        if (docsIndex.isLoading) return null
+
+        // @ts-expect-error -- YOLO
+        return pathParts.reduce((c, e) => c[e], docsIndex.taxonomy)
+    }, [pathParts])
+
+    const { data, isLoading, error } = useSWR<string, FetcherException>(
+        docsIndex.isLoading ? null : articleFile
+    )
+
+    if (docsIndex.isLoading || isLoading) return <LoadingPage />
+
+    if (error != null) return <ErrorPage errorMessage={error} />
+
+    if (data == null) return <ErrorPage errorMessage={'Invalid data'} />
+
+    return (
+        <Markdown className='external max-w-full text-left text-wrap wrap-normal *:my-2.5'>
+            {data}
+        </Markdown>
+    )
+}
 
 export default KnowledgeBasePage
