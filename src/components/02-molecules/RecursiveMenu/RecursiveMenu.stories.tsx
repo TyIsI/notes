a2b979086f41c447d0988528d5c310b98b496dd3
Ty Eggen <tycho.eggen@gmail.com>
2025-08-29 00:53:05 +0000

    fix: 🐛 RecursiveMenu story

--- src/components/02-molecules/RecursiveMenu/RecursiveMenu.stories.tsx
+++ src/components/02-molecules/RecursiveMenu/RecursiveMenu.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention -- YOLO */
 import type { Meta, StoryObj } from '@storybook/react-vite'
 
 import RecursiveMenu from './RecursiveMenu'
@@ -11,5 +12,27 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
-    args: {}
+    args: {
+        taxonomy: {
+            tech: {
+                web: {
+                    css: {
+                        properties: {
+                            'Box Shadow':
+                                '/docs/tech/web/css/properties/box-shadow.md',
+                            'Border Radius':
+                                '/docs/tech/web/css/properties/border-radius.md'
+                        }
+                    }
+                }
+            },
+            science: {
+                mathematics: {
+                    trigonometry: {
+                        Angles: '/docs/science/mathematics/trigonometry/angles.md'
+                    }
+                }
+            }
+        }
+    }
 }
